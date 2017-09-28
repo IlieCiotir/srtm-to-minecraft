f690fc7da57a282af7b7abb97e357e1e4077613f
Ilie Ciotir(Toskv) <dipixelhunter@gmail.com>
2017-09-28 22:17:15 +0300

    use callbacks and setTimeout to stagger building and not overwhelm the server

--- real-world.js
+++ real-world.js
@@ -1,37 +1,64 @@
 function buildWorld(name) {
     var config = loadConfig();
-    generateWorld(config.prefix, config.lines);
-}
-
-function generateWorld(prefix, lines) {
     var worldBuilder = new Drone(self);
-    for (var line = 0; line < lines; line++) {
-        generateLine(readLine(prefix, line), worldBuilder);
-        worldBuilder.right();
-    }
+    generateWorldRecursive(worldBuilder, config.prefix, config.lines, 0);
 }
 
-function generateLine(data, worldBuilder) {
-    worldBuilder.up().box(blocks.dirt);
-    for (var column in data) {
-        generateColumn(column, worldBuilder);
+function generateWorldRecursive(worldBuilder, prefix, lines, current) {
+    line(readLine(prefix, current), worldBuilder, function () {
+        if (current < lines) {
+            worldBuilder.right();
+            generateWorldRecursive(worldBuilder, prefix, lines, current + 1);
+        }
+    });
+}
+
+function line(data, worldBuilder, lineDone) {
+    var queue = new Queue();
+    data.forEach(function (column) {
+        queue.add(columnAction(column, worldBuilder));
+    });
+
+    queue.doAll(function () {
+        worldBuilder.back(data.length);
+        lineDone();
+    });
+}
+
+function columnAction(height, worldBuilder) {
+    return function () {
         worldBuilder.fwd();
-    }
-    worldBuilder.back(data.length);
+        for (var i = 1; i <= height; i += 1) {
+            worldBuilder.box(blocks.dirt).up();
+        }
+        worldBuilder.down(height);
+    };
 }
 
-function generateColumn(height, worldBuilder) {
-    echo("Generating " + height);
-    for (var i = 1; i <= height; i += 1) {
-        worldBuilder.box(blocks.dirt).up();
-    }
-    worldBuilder.down(height);
-}
 function loadConfig() {
     return scload(__dirname + "/output/config.json");
 }
+
 function readLine(prefix, line) {
     return scload(__dirname + "/output/" + prefix + "_" + line + ".json", true);
+}
+
+function Queue() {
+    var actions = [];
+
+    this.add = function (action) {
+        actions.push(action);
+    };
+
+    this.doAll = function (onDone) {
+        setInterval(function () {
+            if (actions.length == 0) {
+                onDone();
+            } else {
+                actions.pop()();
+            }
+        }, 100);
+    };
 }
 
 exports.buildWorld = buildWorld;
